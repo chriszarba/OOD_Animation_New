500774161e0928480c69c5cdb884ea8d75ed7d99
chriszarba <cjzarba1@aol.com>
2019-06-05 11:49:42 -0400

    "added shape stuff"

---
+++ src/AbstractShape.java
@@ -0,0 +1,64 @@
+import java.awt.*;
+
+public class AbstractShape implements IShape {
+
+  @Override
+  public void setX(int x) {
+
+  }
+
+  @Override
+  public void setY(int y) {
+
+  }
+
+  @Override
+  public int getX() {
+    return 0;
+  }
+
+  @Override
+  public int getY() {
+    return 0;
+  }
+
+  @Override
+  public void setVisible(boolean visible) {
+
+  }
+
+  @Override
+  public boolean getVisible() {
+    return false;
+  }
+
+  @Override
+  public void setColor(Color c) {
+
+  }
+
+  @Override
+  public Color getColor() {
+    return null;
+  }
+
+  @Override
+  public void setWidth(int width) {
+
+  }
+
+  @Override
+  public void setHeight(int height) {
+
+  }
+
+  @Override
+  public int getWidth() {
+    return 0;
+  }
+
+  @Override
+  public int getHeight() {
+    return 0;
+  }
+}
--- src/OurModel.java
+++ src/OurModel.java
@@ -1,8 +1,26 @@
 import java.awt.Color;
+import java.awt.geom.Point2D;
 import java.util.List;
 import java.util.Map;
 
 public class OurModel implements IModel {
   Map<String, IShape> shapesMap;
   Map<String, List<IMotion>> motionsMap;
+
+  @Override
+  public void addShape(String name, ShapeType type, Point2D pos, int width, int height,
+                       Color color) {
+
+  }
+
+  @Override
+  public void removeShape(String name) {
+
+  }
+
+  @Override
+  public void addMotion(String name, int t0, int t1, Point2D startPos, Point2D endPos,
+                        int startWidth, int startLength, int endWidth, int endLength, Color startColor, Color endColor) {
+
+  }
 }
