36c8fbc9d30f872c8ace79beff003064e4196e47
Neil Resnik <neilr22@comcast.net>
2019-06-05 15:14:41 -0400

    Minor changes

--- src/IModel.java
+++ src/IModel.java
@@ -9,12 +9,4 @@ public interface IModel {
 
   boolean addMotion(String name, int t0, int t1, Point2D startPos, Point2D endPos, int startWidth,
       int startHeight, int endWidth, int endHeight, Color startColor, Color endColor);
-
-  /*
-  void moveShape(String name, int t0, int t1, int x1, int y1);
-  void visibleShape(String name, int t0, int t1, boolean visible);
-  void resizeShape(String name, int t0, int t1, int newWidth, int newHeight);
-  void changeColorShape(String name, int t0, int t1, Color newColor);
-  List<IShape> getShapes();
-   */
 }
