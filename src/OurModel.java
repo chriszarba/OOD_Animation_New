b6a81547cc2b92ca8da005f6470c77518a92c3c7
Neil Resnik <neilr22@comcast.net>
2019-06-07 17:07:14 -0400

    Added comment discussing design decisions and justifications

--- src/OurModel.java
+++ src/OurModel.java
@@ -8,6 +8,45 @@ import java.util.Map;
 /**
  * An implementation of {@link IModel}. Stores the shapes and motions for the animation, is the
  * model of the model-view-controller design scheme.
+ * Design Decisions and Justification:
+ * <p>To associate shapes with a name, and motions with the shape,
+ * we used HashMaps, due to the fact they have good performance characteristics for this (O(1)
+ * insert/read, order unimportant). The names aren't stored in the motion or shape, because they are
+ * only associated with the shape in this model. For example if a user wants to call the same shape
+ * something else in a different class, that is possible in this implementation. Furthermore the
+ * motions don't store a reference to the shape they refer to for the same reason, and because this
+ * model handles the animation, which is the only thing both are required for. In this
+ * implementation they are loosely coupled, allowing for greater flexibility in the future. The
+ * motions are stored in a list that the name is associated with in the map. This is because a
+ * single shape may have multiple associated motions, and the order of these motions does matter.
+ * Therefore this allows us to keep the motions sorted by starting tick, using insertion sort (which
+ * inserts in O(n) time, keeping the list sorted). This means they are already in chronological
+ * order, which both makes it easier to find the correct motion for a given tick. Furthermore
+ * motions and shapes are constructed internally, which means that the user does not have to worry
+ * about their constructions. This is because they are implementation details, that is they are only
+ * relevant to how the model fulfills its requirements, they are not essentially to fulfilling the
+ * requirements. This means a User has less reliance on specific interfaces or classes, and allows
+ * for easier refactoring of the models internal code without changing its external methods. It also
+ * allows for better error checking, preventing a user from inputting a shape that may be invalid
+ * for this model's assumptions. For example, our IShape interface does not put constraints on what
+ * positions are valid. We made the assumption that the upper left was (0,0), and all coordinates
+ * are positive numbers (as is standard in computer graphics). By constructing the shapes and
+ * motions internally, this prevents the user from passing in a shape or motion with negative
+ * coordinates. Also the coordinates are not locked to what part of the shape they represent. That
+ * is something the view needs to worry about, not the model, and so the model makes no assumptions
+ * about what the coordinates of a shape represent. </p>
+ *
+ * <p>We also used Read-Only interfaces for both the model and shapes, which are extended to provide
+ * mutable functions. This is due to the fact that only certain other classes should manipulate
+ * shapes or the model, allowing someone to choose what classes are allowed to mutate the model. Our
+ * model only returns the immutable shapes, as it does the calculations for the correct fields of
+ * the shape using the given motions and tick, and so the shapes should not be changed outside of
+ * the model. If they need to be a user can construct copies of the shapes, which further decouples
+ * it from the model by allowing them more flexibility in class choice. Rather than throwing
+ * exceptions for invalid shapes or motions, we simply return a boolean that describes the success
+ * of the operation. This means the user does not have to use as many try-catch blocks (as throwing
+ * exceptions is costly in performance) and allows a clear success or failure condition to proceed
+ * on. </p>
  */
 public class OurModel implements IModel {
 
@@ -156,24 +195,6 @@ public class OurModel implements IModel {
                 motion.getInitialColor().getRed(),
                 motion.getInitialColor().getGreen(), motion.getInitialColor().getBlue()));
 
-        /*
-        builder.append(motion.getStartTick());
-        builder.append(" ");
-        builder.append(motion.getInitialPos().getX());
-        builder.append(" ");
-        builder.append(motion.getInitialPos().getY());
-        builder.append(" ");
-        builder.append(motion.getInitialWidth());
-        builder.append(" ");
-        builder.append(motion.getInitialHeight());
-        builder.append(" ");
-        builder.append(motion.getInitialColor().getRed());
-        builder.append(" ");
-        builder.append(motion.getInitialColor().getGreen());
-        builder.append(" ");
-        builder.append(motion.getInitialColor().getBlue());
-         */
-
         builder.append("   ");
 
         builder.append(
@@ -183,23 +204,6 @@ public class OurModel implements IModel {
                 motion.getFinalColor().getRed(),
                 motion.getFinalColor().getGreen(), motion.getFinalColor().getBlue()));
 
-        /*
-        builder.append(motion.getEndTick());
-        builder.append(" ");
-        builder.append(motion.getFinalPos().getX());
-        builder.append(" ");
-        builder.append(motion.getFinalPos().getY());
-        builder.append(" ");
-        builder.append(motion.getFinalWidth());
-        builder.append(" ");
-        builder.append(motion.getFinalHeight());
-        builder.append(" ");
-        builder.append(motion.getFinalColor().getRed());
-        builder.append(" ");
-        builder.append(motion.getFinalColor().getGreen());
-        builder.append(" ");
-        builder.append(motion.getFinalColor().getBlue());
-         */
         builder.append("\n");
       }
       builder.append("\n\n");
